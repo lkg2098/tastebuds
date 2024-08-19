ad5cf98da5507967a673107e6e0e7771eadd8c51
Lauren Gray <laurengray98@gmail.com>
2024-08-19 12:16:45 -0400

    added error listeners to diagnose websocket problem

--- server/server.js
+++ server/server.js
@@ -42,6 +42,13 @@ app.closeServer = () => {
   server.close();
 };
 
+io.engine.on("connection_error", (err) => {
+  console.log(err.req); // the request object
+  console.log(err.code); // the error code, for example 1
+  console.log(err.message); // the error message, for example "Session ID unknown"
+  console.log(err.context); // some additional error context
+});
+
 io.on("connection", (socket) => {
   console.log("user connected");
   socket.on("joinMeal", (mealId, memberId) => {
