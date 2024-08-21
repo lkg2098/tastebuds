f5c9ab4b046f709f669c28b4f2777895e47e2bf2
Lauren Gray <laurengray98@gmail.com>
2024-08-21 17:16:52 -0400

    remove function calls from sms file

--- server/sms.js
+++ server/sms.js
@@ -3,18 +3,4 @@ const sms = require("twilio")(
   process.env.TWILIO_AUTH_TOKEN
 );
 
-sms.sendCode = (phone_number) => {
-  sms.verify.v2
-    .services(process.env.TWILIO_VERIFY_SID)
-    .verifications.create({ to: phone_number, channel: "sms" })
-    .then((verification) => console.log(verification.sid));
-};
-
-sms.verifyCode = (phone_number, code) => {
-  sms.verify.v2
-    .services(process.env.TWILIO_VERIFY_SID)
-    .verificationChecks.create({ to: phone_number, code: code })
-    .then((verification_check) => console.log(verification_check.status));
-};
-
 module.exports = sms;
